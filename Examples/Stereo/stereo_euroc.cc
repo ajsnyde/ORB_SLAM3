0f2ce5d081269ce6fdcef02e9d3b49677057e62c
Addison Snyder <addison@addisonsnyder.com>
2023-02-19 19:19:41 -0500

    Added stereo file reading

--- Examples/Stereo/stereo_euroc.cc
+++ Examples/Stereo/stereo_euroc.cc
@@ -15,7 +15,7 @@
 * You should have received a copy of the GNU General Public License along with ORB-SLAM3.
 * If not, see <http://www.gnu.org/licenses/>.
 */
-
+#include <cstdlib>
 #include<iostream>
 #include<algorithm>
 #include<fstream>
@@ -27,218 +27,251 @@
 #include<System.h>
 
 using namespace std;
+using namespace cv;
 
+int c = 0;
+string int2str(int &);
 void LoadImages(const string &strPathLeft, const string &strPathRight, const string &strPathTimes,
                 vector<string> &vstrImageLeft, vector<string> &vstrImageRight, vector<double> &vTimeStamps);
 
-int main(int argc, char **argv)
+int main(int argc, const char *argv[])
 {  
-    if(argc < 5)
-    {
-        cerr << endl << "Usage: ./stereo_euroc path_to_vocabulary path_to_settings path_to_sequence_folder_1 path_to_times_file_1 (path_to_image_folder_2 path_to_times_file_2 ... path_to_image_folder_N path_to_times_file_N) (trajectory_file_name)" << endl;
+	if (argc < 4) {
+		cerr << endl
+				<< "Usage: ./mono_euroc path_to_vocabulary path_to_settings path_to_left_video_file path_to_right_video_file left_frames_to_discard_may_be_negative) (trajectory_file_name)"
+				<< endl;
+		return 1;
+	}
 
-        return 1;
-    }
+	// Create SLAM system. It initializes all system threads and gets ready to process frames.
+	ORB_SLAM3::System SLAM(argv[1], argv[2], ORB_SLAM3::System::STEREO,
+			true);
 
-    const int num_seq = (argc-3)/2;
-    cout << "num_seq = " << num_seq << endl;
-    bool bFileName= (((argc-3) % 2) == 1);
-    string file_name;
-    if (bFileName)
-    {
-        file_name = string(argv[argc-1]);
-        cout << "file name: " << file_name << endl;
-    }
+	std::string leftVideoFilePath = argv[3];
+	std::string rightVideoFilePath = argv[4];
+	VideoCapture leftCap(leftVideoFilePath); // video
+	sleep(1);
+	if (!leftCap.isOpened()) {
+		cout << "Cannot open the video file: " + leftVideoFilePath << endl;
+		return -1;
+	}
+	double leftFps = leftCap.get(CV_CAP_PROP_FPS); //get the frames per seconds of the video
 
-    // Load all sequences:
-    int seq;
-    vector< vector<string> > vstrImageLeft;
-    vector< vector<string> > vstrImageRight;
-    vector< vector<double> > vTimestampsCam;
-    vector<int> nImages;
+	VideoCapture rightCap(rightVideoFilePath); // video
+	if (!rightCap.isOpened()) {
+		cout << "Cannot open the video file: " + rightVideoFilePath << endl;
+		return -1;
+	}
+	double rightFps = rightCap.get(CV_CAP_PROP_FPS); //get the frames per seconds of the video
 
-    vstrImageLeft.resize(num_seq);
-    vstrImageRight.resize(num_seq);
-    vTimestampsCam.resize(num_seq);
-    nImages.resize(num_seq);
+	cout << "Frame per seconds - Left: " << leftFps << " Right: " << rightFps << endl;
 
-    int tot_images = 0;
-    for (seq = 0; seq<num_seq; seq++)
-    {
-        cout << "Loading images for sequence " << seq << "...";
+	//namedWindow("MyVideo", CV_WINDOW_AUTOSIZE); //create a window called "MyVideo"
 
-        string pathSeq(argv[(2*seq) + 3]);
-        string pathTimeStamps(argv[(2*seq) + 4]);
+	//std::string offsetString << argv[4];
+	int offset = atoi(argv[4]);
 
-        string pathCam0 = pathSeq + "/mav0/cam0/data";
-        string pathCam1 = pathSeq + "/mav0/cam1/data";
+	cv::Mat leftFrame;
+	cv::Mat rightFrame;
+	// offset frame stream by x frames; negative indicates to toss right-frames, positive to toss left
+	for(int i = 0; i < abs(offset); i++){
+		if(offset < 0){
+			rightCap.read(rightFrame);
+		} else if (offset > 0){
+			leftCap.read(leftFrame);
+		}
+	}
 
-        LoadImages(pathCam0, pathCam1, pathTimeStamps, vstrImageLeft[seq], vstrImageRight[seq], vTimestampsCam[seq]);
-        cout << "LOADED!" << endl;
+	for (double i = 0; true; i++) {
+		bool leftSuccess = leftCap.read(leftFrame); // read a new frame from video
+		bool rightSuccess = rightCap.read(rightFrame); // read a new frame from video
 
-        nImages[seq] = vstrImageLeft[seq].size();
-        tot_images += nImages[seq];
-    }
+		if (!leftSuccess) {
+			cout << "Cannot read the frame from video file: " + leftVideoFilePath
+					<< endl;
+			break;
+		}
+		if (!rightSuccess) {
+			cout << "Cannot read the frame from video file: " + rightVideoFilePath
+					<< endl;
+			break;
+		}
 
-    // Read rectification parameters
-    cv::FileStorage fsSettings(argv[2], cv::FileStorage::READ);
-    if(!fsSettings.isOpened())
-    {
-        cerr << "ERROR: Wrong path to settings" << endl;
-        return -1;
-    }
+		//imshow("MyVideo", frame); //show the frame in "MyVideo" window
+		//imwrite("ig" + std::to_string(i) + ".jpg", frame);
 
-    cv::Mat K_l, K_r, P_l, P_r, R_l, R_r, D_l, D_r;
-    fsSettings["LEFT.K"] >> K_l;
-    fsSettings["RIGHT.K"] >> K_r;
+		// Pass the image to the SLAM system
+		SLAM.TrackStereo(leftFrame, rightFrame, i * (1 / leftFps));
 
-    fsSettings["LEFT.P"] >> P_l;
-    fsSettings["RIGHT.P"] >> P_r;
-
-    fsSettings["LEFT.R"] >> R_l;
-    fsSettings["RIGHT.R"] >> R_r;
-
-    fsSettings["LEFT.D"] >> D_l;
-    fsSettings["RIGHT.D"] >> D_r;
-
-    int rows_l = fsSettings["LEFT.height"];
-    int cols_l = fsSettings["LEFT.width"];
-    int rows_r = fsSettings["RIGHT.height"];
-    int cols_r = fsSettings["RIGHT.width"];
-
-    if(K_l.empty() || K_r.empty() || P_l.empty() || P_r.empty() || R_l.empty() || R_r.empty() || D_l.empty() || D_r.empty() ||
-            rows_l==0 || rows_r==0 || cols_l==0 || cols_r==0)
-    {
-        cerr << "ERROR: Calibration parameters to rectify stereo are missing!" << endl;
-        return -1;
-    }
-
-    cv::Mat M1l,M2l,M1r,M2r;
-    cv::initUndistortRectifyMap(K_l,D_l,R_l,P_l.rowRange(0,3).colRange(0,3),cv::Size(cols_l,rows_l),CV_32F,M1l,M2l);
-    cv::initUndistortRectifyMap(K_r,D_r,R_r,P_r.rowRange(0,3).colRange(0,3),cv::Size(cols_r,rows_r),CV_32F,M1r,M2r);
+		if (waitKey(30) == 27) //esc key
+				{
+			cout << "esc key is pressed by user" << endl;
+			break;
+		}
+	}
 
 
-    // Vector for tracking time statistics
-    vector<float> vTimesTrack;
-    vTimesTrack.resize(tot_images);
-
-    cout << endl << "-------" << endl;
-    cout.precision(17);
-
-    // Create SLAM system. It initializes all system threads and gets ready to process frames.
-    ORB_SLAM3::System SLAM(argv[1],argv[2],ORB_SLAM3::System::STEREO, true);
-
-    cv::Mat imLeft, imRight, imLeftRect, imRightRect;
-    for (seq = 0; seq<num_seq; seq++)
-    {
-
-        // Seq loop
-
-        double t_rect = 0;
-        double t_track = 0;
-        int num_rect = 0;
-        int proccIm = 0;
-        for(int ni=0; ni<nImages[seq]; ni++, proccIm++)
-        {
-            // Read left and right images from file
-            imLeft = cv::imread(vstrImageLeft[seq][ni],cv::IMREAD_UNCHANGED);
-            imRight = cv::imread(vstrImageRight[seq][ni],cv::IMREAD_UNCHANGED);
-
-            if(imLeft.empty())
-            {
-                cerr << endl << "Failed to load image at: "
-                     << string(vstrImageLeft[seq][ni]) << endl;
-                return 1;
-            }
-
-            if(imRight.empty())
-            {
-                cerr << endl << "Failed to load image at: "
-                     << string(vstrImageRight[seq][ni]) << endl;
-                return 1;
-            }
-
-#ifdef REGISTER_TIMES
-    #ifdef COMPILEDWITHC11
-            std::chrono::steady_clock::time_point t_Start_Rect = std::chrono::steady_clock::now();
-    #else
-            std::chrono::monotonic_clock::time_point t_Start_Rect = std::chrono::monotonic_clock::now();
-    #endif
-#endif
-            cv::remap(imLeft,imLeftRect,M1l,M2l,cv::INTER_LINEAR);
-            cv::remap(imRight,imRightRect,M1r,M2r,cv::INTER_LINEAR);
-
-#ifdef REGISTER_TIMES
-    #ifdef COMPILEDWITHC11
-            std::chrono::steady_clock::time_point t_End_Rect = std::chrono::steady_clock::now();
-    #else
-            std::chrono::monotonic_clock::time_point t_End_Rect = std::chrono::monotonic_clock::now();
-    #endif
-            t_rect = std::chrono::duration_cast<std::chrono::duration<double,std::milli> >(t_End_Rect - t_Start_Rect).count();
-            SLAM.InsertRectTime(t_rect);
-#endif
-            double tframe = vTimestampsCam[seq][ni];
-    #ifdef COMPILEDWITHC11
-            std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
-    #else
-            std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
-    #endif
-
-            // Pass the images to the SLAM system
-            SLAM.TrackStereo(imLeftRect,imRightRect,tframe, vector<ORB_SLAM3::IMU::Point>(), vstrImageLeft[seq][ni]);
-
-    #ifdef COMPILEDWITHC11
-            std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
-    #else
-            std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
-    #endif
-
-#ifdef REGISTER_TIMES
-            t_track = t_rect + std::chrono::duration_cast<std::chrono::duration<double,std::milli> >(t2 - t1).count();
-            SLAM.InsertTrackTime(t_track);
-#endif
-
-            double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
-
-            vTimesTrack[ni]=ttrack;
-
-            // Wait to load the next frame
-            double T=0;
-            if(ni<nImages[seq]-1)
-                T = vTimestampsCam[seq][ni+1]-tframe;
-            else if(ni>0)
-                T = tframe-vTimestampsCam[seq][ni-1];
-
-            if(ttrack<T)
-                usleep((T-ttrack)*1e6);
-        }
-
-        if(seq < num_seq - 1)
-        {
-            cout << "Changing the dataset" << endl;
-
-            SLAM.ChangeDataset();
-        }
-
-    }
+//
+//    // Read rectification parameters
+//    cv::FileStorage fsSettings(argv[2], cv::FileStorage::READ);
+//    if(!fsSettings.isOpened())
+//    {
+//        cerr << "ERROR: Wrong path to settings" << endl;
+//        return -1;
+//    }
+//
+//    cv::Mat K_l, K_r, P_l, P_r, R_l, R_r, D_l, D_r;
+//    fsSettings["LEFT.K"] >> K_l;
+//    fsSettings["RIGHT.K"] >> K_r;
+//
+//    fsSettings["LEFT.P"] >> P_l;
+//    fsSettings["RIGHT.P"] >> P_r;
+//
+//    fsSettings["LEFT.R"] >> R_l;
+//    fsSettings["RIGHT.R"] >> R_r;
+//
+//    fsSettings["LEFT.D"] >> D_l;
+//    fsSettings["RIGHT.D"] >> D_r;
+//
+//    int rows_l = fsSettings["LEFT.height"];
+//    int cols_l = fsSettings["LEFT.width"];
+//    int rows_r = fsSettings["RIGHT.height"];
+//    int cols_r = fsSettings["RIGHT.width"];
+//
+//    if(K_l.empty() || K_r.empty() || P_l.empty() || P_r.empty() || R_l.empty() || R_r.empty() || D_l.empty() || D_r.empty() ||
+//            rows_l==0 || rows_r==0 || cols_l==0 || cols_r==0)
+//    {
+//        cerr << "ERROR: Calibration parameters to rectify stereo are missing!" << endl;
+//        return -1;
+//    }
+//
+//    cv::Mat M1l,M2l,M1r,M2r;
+//    cv::initUndistortRectifyMap(K_l,D_l,R_l,P_l.rowRange(0,3).colRange(0,3),cv::Size(cols_l,rows_l),CV_32F,M1l,M2l);
+//    cv::initUndistortRectifyMap(K_r,D_r,R_r,P_r.rowRange(0,3).colRange(0,3),cv::Size(cols_r,rows_r),CV_32F,M1r,M2r);
+//
+//
+//    // Vector for tracking time statistics
+//    vector<float> vTimesTrack;
+//    vTimesTrack.resize(tot_images);
+//
+//    cout << endl << "-------" << endl;
+//    cout.precision(17);
+//
+//    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+//    ORB_SLAM3::System SLAM(argv[1],argv[2],ORB_SLAM3::System::STEREO, true);
+//
+//    cv::Mat imLeft, imRight, imLeftRect, imRightRect;
+//    for (seq = 0; seq<num_seq; seq++)
+//    {
+//
+//        // Seq loop
+//
+//        double t_rect = 0;
+//        double t_track = 0;
+//        int num_rect = 0;
+//        int proccIm = 0;
+//        for(int ni=0; ni<nImages[seq]; ni++, proccIm++)
+//        {
+//            // Read left and right images from file
+//            imLeft = cv::imread(vstrImageLeft[seq][ni],cv::IMREAD_UNCHANGED);
+//            imRight = cv::imread(vstrImageRight[seq][ni],cv::IMREAD_UNCHANGED);
+//
+//            if(imLeft.empty())
+//            {
+//                cerr << endl << "Failed to load image at: "
+//                     << string(vstrImageLeft[seq][ni]) << endl;
+//                return 1;
+//            }
+//
+//            if(imRight.empty())
+//            {
+//                cerr << endl << "Failed to load image at: "
+//                     << string(vstrImageRight[seq][ni]) << endl;
+//                return 1;
+//            }
+//
+//#ifdef REGISTER_TIMES
+//    #ifdef COMPILEDWITHC11
+//            std::chrono::steady_clock::time_point t_Start_Rect = std::chrono::steady_clock::now();
+//    #else
+//            std::chrono::monotonic_clock::time_point t_Start_Rect = std::chrono::monotonic_clock::now();
+//    #endif
+//#endif
+//            cv::remap(imLeft,imLeftRect,M1l,M2l,cv::INTER_LINEAR);
+//            cv::remap(imRight,imRightRect,M1r,M2r,cv::INTER_LINEAR);
+//
+//#ifdef REGISTER_TIMES
+//    #ifdef COMPILEDWITHC11
+//            std::chrono::steady_clock::time_point t_End_Rect = std::chrono::steady_clock::now();
+//    #else
+//            std::chrono::monotonic_clock::time_point t_End_Rect = std::chrono::monotonic_clock::now();
+//    #endif
+//            t_rect = std::chrono::duration_cast<std::chrono::duration<double,std::milli> >(t_End_Rect - t_Start_Rect).count();
+//            SLAM.InsertRectTime(t_rect);
+//#endif
+//            double tframe = vTimestampsCam[seq][ni];
+//    #ifdef COMPILEDWITHC11
+//            std::chrono::steady_clock::time_point t1 = std::chrono::steady_clock::now();
+//    #else
+//            std::chrono::monotonic_clock::time_point t1 = std::chrono::monotonic_clock::now();
+//    #endif
+//
+//            // Pass the images to the SLAM system
+//            SLAM.TrackStereo(imLeftRect,imRightRect,tframe, vector<ORB_SLAM3::IMU::Point>(), vstrImageLeft[seq][ni]);
+//
+//    #ifdef COMPILEDWITHC11
+//            std::chrono::steady_clock::time_point t2 = std::chrono::steady_clock::now();
+//    #else
+//            std::chrono::monotonic_clock::time_point t2 = std::chrono::monotonic_clock::now();
+//    #endif
+//
+//#ifdef REGISTER_TIMES
+//            t_track = t_rect + std::chrono::duration_cast<std::chrono::duration<double,std::milli> >(t2 - t1).count();
+//            SLAM.InsertTrackTime(t_track);
+//#endif
+//
+//            double ttrack= std::chrono::duration_cast<std::chrono::duration<double> >(t2 - t1).count();
+//
+//            vTimesTrack[ni]=ttrack;
+//
+//            // Wait to load the next frame
+//            double T=0;
+//            if(ni<nImages[seq]-1)
+//                T = vTimestampsCam[seq][ni+1]-tframe;
+//            else if(ni>0)
+//                T = tframe-vTimestampsCam[seq][ni-1];
+//
+//            if(ttrack<T)
+//                usleep((T-ttrack)*1e6);
+//        }
+//
+//        if(seq < num_seq - 1)
+//        {
+//            cout << "Changing the dataset" << endl;
+//
+//            SLAM.ChangeDataset();
+//        }
+//
+//    }
     // Stop all threads
     SLAM.Shutdown();
 
     // Save camera trajectory
-    if (bFileName)
-    {
-        const string kf_file =  "kf_" + string(argv[argc-1]) + ".txt";
-        const string f_file =  "f_" + string(argv[argc-1]) + ".txt";
-        SLAM.SaveTrajectoryEuRoC(f_file);
-        SLAM.SaveKeyFrameTrajectoryEuRoC(kf_file);
-    }
-    else
-    {
-        SLAM.SaveTrajectoryEuRoC("CameraTrajectory.txt");
-        SLAM.SaveKeyFrameTrajectoryEuRoC("KeyFrameTrajectory.txt");
-    }
+
+	const string kf_file =  "kf_" + string(argv[argc-1]) + ".txt";
+	const string f_file =  "f_" + string(argv[argc-1]) + ".txt";
+	SLAM.SaveTrajectoryEuRoC(f_file);
+	SLAM.SaveKeyFrameTrajectoryEuRoC(kf_file);
+
 
     return 0;
+}
+
+string int2str(int &i) {
+    string s;
+    stringstream ss(s);
+    ss << i;
+    return ss.str();
 }
 
 void LoadImages(const string &strPathLeft, const string &strPathRight, const string &strPathTimes,
